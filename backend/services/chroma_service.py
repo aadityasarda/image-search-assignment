49d665787b8517bfbb40445732708187729eccda
aadityasarda <adityasarda2004@gmail.com>
2025-06-14 18:26:34 +0530

    update chroma_service.py

--- backend/services/chroma_service.py
+++ backend/services/chroma_service.py
@@ -1,5 +1,5 @@
 #this file is used to load the feature vectors store in .npy files into chromadb
-import os
+
 import numpy as np
 from config import FEATURES_PATH, IMAGE_PATHS_PATH
 from utils.chroma_client import get_chroma_collection
@@ -9,33 +9,27 @@ def get_label_from_path(path: str) -> str:
     return path.split('/')[-2]
 
 def load_embeddings_into_chroma():
+    import time
+    start = time.time()
+    print("Loading features and image paths")
 
     features = np.load(FEATURES_PATH)
     image_paths = np.load(IMAGE_PATHS_PATH)
 
     if len(features) != len(image_paths):
-        raise ValueError("Mismatch between features and image paths.")
+        raise ValueError("Mismatch between features and image paths")
+
+    print(f"Loaded {len(features)} features")
 
     collection = get_chroma_collection()
 
-    for i in range(len(features)):
-        embedding = features[i].tolist()
-        original_path = str(image_paths[i])
-        label = get_label_from_path(original_path)
-        filename = os.path.basename(original_path)
+    ids = [f"img_{i}" for i in range(len(features))]
+    embeddings = [features[i].tolist() for i in range(len(features))]
+    metadatas = [{
+        "url": str(image_paths[i]),
+        "label": get_label_from_path(str(image_paths[i]))
+    } for i in range(len(features))]
 
-        url = f"data/resized/{label}/{filename}"
-
-        collection.add(
-            ids=[f"img_{i}"],
-            embeddings=[embedding],
-            metadatas=[{
-                "url": url,
-                "label": label
-            }]
-        )
-
-        if i < 3:  # Optional: log first few
-            print(f" img_{i}: {label} → {url}")
-
-    print(f" Loaded {len(features)} vectors into ChromaDB.")
+    collection.add(ids=ids, embeddings=embeddings, metadatas=metadatas)
+    print(f"Successfully inserted {len(embeddings)} vectors into ChromaDB.")
+    print(f"Total load time: {round(time.time() - start, 2)}s")
